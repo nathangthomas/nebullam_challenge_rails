a0dffb61760517fe997e3c47ce949e4a92d76f05
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-05-20 15:10:22 -0600

    Delete challenge_data model

--- app/models/challenge_data.rb
+++
@@ -1,9 +0,0 @@
-class ChallengeData
-  attr_reader :id, :position, :is_calibrated
-
-  def initialize(data)
-    @id = data[:servoId]
-    @position = data[:position]
-    @is_calibrated = data[:isCalibrated]
-  end
-end
